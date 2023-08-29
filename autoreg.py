c146ddcfa8feec9cec472676e4c93ba822a9d70d
doolbarez <106016221+doolbarez@users.noreply.github.com>
2023-08-29 17:24:04 +0900

    Update autoreg.py

--- autoreg.py
+++ autoreg.py
@@ -1,12 +1,26 @@
-from selenium import webdriver
+from seleniumwire import webdriver
+from seleniumwire.utils import decode
+from image_detector import solve_image
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from fake_useragent import UserAgent
 import time
-import requests
+import requests as rs
 import random
 import string
+import json
+from urllib.parse import quote
+
+def get_solutions(cs):
+    try:
+        response = rs.post("http://127.0.0.1:8000/solve", json={
+            "challenges": cs
+        })
+        response.raise_for_status()  # Проверка статуса ответа
+        return response.json().get('solutions', [])
+    except (rs.exceptions.RequestException, ValueError):
+        return []
 
 # Настройка опций для веб-драйвера
 
@@ -17,21 +31,31 @@ with open('Useragent.txt', 'r') as file:
 random_useragent = random.choice(useragents)
 
 # Использование случайного User-Agent в Selenium WebDriver
-UserAgent = UserAgent()
-options = webdriver.ChromeOptions()
-options.add_argument("--incognito") 
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument(f"user-agent={random_useragent}")
-# Тут добавить функцию proxy листов
+seleniumwire_options = webdriver.ChromeOptions()
+seleniumwire_options .add_argument("--disable-blink-features=AutomationControlled")
+seleniumwire_options .add_argument(f"user-agent={random_useragent}")
+seleniumwire_options .add_argument('--ignore-certificate-errors-spki-list')
+seleniumwire_options .add_argument('--ignore-ssl-errors')
+options= {
+    'proxy': {
+        'http': 'http://brd-customer-hl_ce63cbe3-zone-data_center:47go741wi9a6@brd.superproxy.io:22225',
+        'https': 'http://brd-customer-hl_ce63cbe3-zone-data_center:47go741wi9a6@brd.superproxy.io:22225',
+        'no_proxy': 'localhost,127.0.0.1:8000'
+    }
+}
+proxies = {
+    'http': 'http://brd-customer-hl_ce63cbe3-zone-data_center:47go741wi9a6@brd.superproxy.io:22225',
+    'https': 'http://brd-customer-hl_ce63cbe3-zone-data_center:47go741wi9a6@brd.superproxy.io:22225',
+    'no_proxy': 'localhost,127.0.0.1:8000'
+}
 # Запуск веб-драйвера с настройками прокси
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(seleniumwire_options=options)
 # Открытие страницы
 driver.get("https://account.proton.me/mail/signup")
 time.sleep(7)
 
-
 # Переключение на фрейм
-frame = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[4]/div[1]/main/div[1]/div[2]/form/iframe")))
+frame = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[4]/div[1]/main/div[1]/div[2]/form/iframe")))
 driver.switch_to.frame(frame)
 # Нахождение полей и ввод данных
 email_field = driver.find_element(By.ID, "email")
@@ -55,28 +79,28 @@ create_button = driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/div[1]/m
 create_button.click()
 time.sleep(2)
 free_button = driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/div/main/div/div[1]/div[2]/div[3]/button")
+driver.scopes = ['.*']
+requests = driver.requests
+token = None
+contest_id = None
+challenges = None
+def intercept_response(request, response):
+    global token, contest_id, challenges
+    if request.url.startswith('https://account-api.proton.me/captcha/v1/api/init?'):
+        response = request.response
+        body = decode(response.body, response.headers.get('Content-Encoding','identity'))
+        decoded_body = body.decode('utf-8')
+        json_data = json.loads(decoded_body)
+        token = json_data['token']
+        contest_id = json_data['contestId']
+        challenges = json_data['challenges']
+driver.response_interceptor = intercept_response
 free_button.click()
-
-import json
-import requests
-from image_detector import solve_image
-
-
-def get_solutions(cs):
-    return requests.post("http://127.0.0.1:8000/solve", json={
-        "challenges": cs
-    }).json()['solutions']
-# Получение токена капчи
-#verify_captcha = driver.find_element(By.XPATH, "Тут найти тег каптчи")
-#if verify_captcha = тут сделать условие если присутствует
-captcha_token = "EuNN2AjxcxgpKAxKCrcgIqwb"  # Значение токена капчи
-
-# Инициализация капчи
-first_request = requests.get(f"https://account-api.proton.me/captcha/v1/api/init?challengeType=2D&parentURL=https://account-api.proton.me/core/v4/captcha?Token={captcha_token}&ForceWebMessaging=1&displayedLang=en&supportedLangs=en-US,en-US,en,en-US,en&purpose=signup")
-
-# Получение изображения капчи
-img = requests.get("https://account-api.proton.me/captcha/v1/api/bg", params={
-    "token": first_request.json()['token']
+waitframe= WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[4]/div/main/div/div[2]/div/div[2]/iframe")))
+time.sleep(7)
+# Окно с капчей
+img = rs.get("https://account-api.proton.me/captcha/v1/api/bg", proxies=proxies, params={
+    "token": token
 })
 
 # Распознавание изображения капчи
@@ -85,9 +109,7 @@ if t is None:
     exit()
 
 # Получение ответов на капчу
-challenges = first_request.json()['challenges']
 answers = get_solutions(challenges)
-
 print(t, answers)
 
 # Составление объекта капчи
@@ -102,23 +124,34 @@ captcha_object = {
     "solveChallengeMs": 5000,
     "powElapsedMs": 540
 }
+pcaptcha = json.dumps(captcha_object)
 
+url = f"https://account-api.proton.me/captcha/v1/api/validate?token={quote(token.encode('utf-8'))}&contestId={quote(contest_id.encode('utf-8'))}&purpose=signup"
+
+headers = {
+    "Content-Type": "application/json",
+    "pcaptcha": pcaptcha
+}
+
+captcha_frame = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[4]/div/main/div/div[2]/div/div[2]/iframe")))
+driver.switch_to.frame(captcha_frame)
+captcha_frame2 = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/iframe")))
+driver.switch_to.frame(captcha_frame2)
 
 next_button = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div/div/div[2]/button[1]")
-activate_script = "arguments[0].removeAttribute('disabled');"
-driver.execute_script(activate_script, next_button)
+file_request = None
+def intercept_response2(request):
+    global file_request
+    # Проверить URL запроса и сохранить его, если он соответствует вашим требованиям
+    if request.url.startswith('https://account-api.proton.me/captcha/v1/api/validate?token'):
+        request.create_response(
+        status_code = 200,
+        headers = {"Content-Type": "application/json",    "pcaptcha": pcaptcha}
+        )
+        file_request = 1
 
-
-# Отправка ответа на капчу для верификации
-submit_request = requests.get("https://account-api.proton.me/captcha/v1/api/validate", params={
-    "token": first_request.json()['token'],
-    "contestId": first_request.json()['contestId'],
-    "purpose": "signup"
-}, headers={
-    "pcaptcha": json.dumps(captcha_object)
-})
-print(submit_request.text)
-driver.execute_script("arguments[0].setAttribute('onclick', arguments[1]);", next_button, submit_request)
+# Установить функцию перехвата запроса перед отправкой
+driver.request_interceptor = intercept_response2
 next_button.click()
-time.sleep(100)
+time.sleep(200)
 driver.quit()
